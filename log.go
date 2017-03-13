5f60627a3f123fab29cc5b74f179a7d26bcce629
Diego Toledo <toledo.diego09@gmail.com>
2017-03-13 13:09:50 -0600

    Fixed spacing

--- log.go
+++ log.go
@@ -2,8 +2,8 @@ package main
 
 // Log defines a model for the logs received.
 type Log struct {
-	Timestamp int64  `json:"timestamp"`
-	Stage     int    `json:"stage"`
-	Message   string `json:"message"`
-	SessionID string `json:"sessionId"`
+  Timestamp int64  `json:"timestamp"`
+  Stage     int    `json:"stage"`
+  Message   string `json:"message"`
+  SessionID string `json:"sessionId"`
 }
--- main.go
+++ main.go
@@ -1,105 +1,103 @@
 package main
 
 import (
-	"log"
-	"os"
-	"os/signal"
-	"sync"
+  "log"
+  "os"
+  "os/signal"
+  "sync"
+  "time"
+  "encoding/json"
 
-	"time"
-
-	"encoding/json"
-
-	"github.com/Shopify/sarama"
+  "github.com/Shopify/sarama"
 )
 
 func generateRandomLog(sessionID string, stage int) []byte {
-	log := &Log{
-		Stage:     stage,
-		Timestamp: time.Now().Unix(),
-		Message:   RandStringBytesMaskImpr(15),
-		SessionID: sessionID,
-	}
+  log := &Log{
+    Stage:     stage,
+    Timestamp: time.Now().Unix(),
+    Message:   RandStringBytesMaskImpr(15),
+    SessionID: sessionID,
+  }
 
-	jsonLog, err := json.Marshal(log)
-	if err != nil {
-		return nil
-	}
+  jsonLog, err := json.Marshal(log)
+  if err != nil {
+    return nil
+  }
 
-	return jsonLog
+  return jsonLog
 }
 
 func sendLog(channel chan<- *sarama.ProducerMessage, log []byte) {
-	channel <- &sarama.ProducerMessage{Topic: "test", Value: sarama.ByteEncoder(log)}
+  channel <- &sarama.ProducerMessage{Topic: "test", Value: sarama.ByteEncoder(log)}
 }
 
 func mockUser(channel chan<- *sarama.ProducerMessage, kill chan os.Signal) {
-	sessionID := RandStringBytesMaskImpr(10)
+  sessionID := RandStringBytesMaskImpr(10)
 
-	for {
-		select {
+  for {
+    select {
 
-		default:
-			sendLog(channel, generateRandomLog(sessionID, 1))
-			sendLog(channel, generateRandomLog(sessionID, 2))
-			sendLog(channel, generateRandomLog(sessionID, 2))
-			sendLog(channel, generateRandomLog(sessionID, 3))
-		}
-	}
+    default:
+      sendLog(channel, generateRandomLog(sessionID, 1))
+      sendLog(channel, generateRandomLog(sessionID, 2))
+      sendLog(channel, generateRandomLog(sessionID, 2))
+      sendLog(channel, generateRandomLog(sessionID, 3))
+    }
+  }
 }
 
 func main() {
-	config := sarama.NewConfig()
-	config.Producer.Return.Successes = true
-	producer, err := sarama.NewAsyncProducer([]string{"localhost:9092"}, config)
-	if err != nil {
-		panic(err)
-	}
+  config := sarama.NewConfig()
+  config.Producer.Return.Successes = true
+  producer, err := sarama.NewAsyncProducer([]string{"localhost:9092"}, config)
 
-	// Trap SIGINT to trigger a graceful shutdown.
-	signals := make(chan os.Signal, 1)
-	signal.Notify(signals, os.Interrupt)
+  if err != nil {
+    panic(err)
+  }
 
-	var (
-		wg                sync.WaitGroup
-		successes, errors int
-	)
+  // Trap SIGINT to trigger a graceful shutdown.
+  signals := make(chan os.Signal, 1)
+  signal.Notify(signals, os.Interrupt)
 
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		for range producer.Successes() {
-			successes++
-		}
-	}()
+  var (
+    wg                sync.WaitGroup
+    successes, errors int
+  )
 
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
-		for err := range producer.Errors() {
-			log.Println(err)
-			errors++
-		}
-	}()
+  wg.Add(1)
+  go func() {
+    defer wg.Done()
+    for range producer.Successes() {
+      successes++
+    }
+  }()
 
-	mockUser(producer.Input(), signals)
-ProducerLoop:
-	for {
+  wg.Add(1)
+  go func() {
+    defer wg.Done()
+    for err := range producer.Errors() {
+      log.Println(err)
+      errors++
+    }
+  }()
 
-		select {
-		case <-signals:
-			producer.AsyncClose() // Trigger a shutdown of the producer.
-			break ProducerLoop
+  mockUser(producer.Input(), signals)
+  ProducerLoop:
+  for {
+    select {
+      case <-signals:
+        producer.AsyncClose() // Trigger a shutdown of the producer.
+        break ProducerLoop
 
-		default:
-			if successes > 100 {
-				producer.AsyncClose() // Trigger a shutdown of the producer.
-				break ProducerLoop
-			}
-		}
-	}
+      default:
+        if successes > 100 {
+          producer.AsyncClose() // Trigger a shutdown of the producer.
+          break ProducerLoop
+        }
+    }
+  }
 
-	wg.Wait()
+  wg.Wait()
 
-	log.Printf("Successfully produced: %d; errors: %d\n", successes, errors)
+  log.Printf("Successfully produced: %d; errors: %d\n", successes, errors)
 }
